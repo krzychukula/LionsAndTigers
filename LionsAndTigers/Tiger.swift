fe2c74bfbc2a729a3d050e9ade224a9fae879b06
krzychukula <krzychukula@gmail.com>
2014-12-14 10:47:06 +0100

    New struct file

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,13 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Krzysztof Kula on 14/12/14.
+//  Copyright (c) 2014 Krzysztof Kula. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+    
+}
