b8a8587ea59a4831b606f57d135852f0cd97a011
krzychukula <krzychukula@gmail.com>
2014-12-15 15:50:32 +0100

    use switch

--- LionsAndTigers/Tiger.swift
+++ LionsAndTigers/Tiger.swift
@@ -45,12 +45,13 @@ struct Tiger {
         
         var randomFact:String
         
-        if randomNumber == 0 {
-            randomFact = "The Tiger is the biggest species in the cat family"
-        }else if randomNumber == 1 {
-            randomFact = "Tigers can reach a length of 3.3 meters"
-        }else{
-            randomFact = "A group of tigers is known as an 'ambush' or 'streak'"
+        switch randomNumber {
+            case 0:
+                randomFact = "The Tiger is the biggest species in the cat family"
+            case 1:
+                randomFact = "Tigers can reach a length of 3.3 meters"
+            default:
+                randomFact = "A group of tigers is known as an 'ambush' or 'streak'"
         }
         
         return randomFact
